474c89862d6eef44760f8dfc02123bb97e8e5dbb
Ana Paz Parietti <166624266+anapazparietti@users.noreply.github.com>
2024-10-16 13:51:24 -0300

    Add files via upload

--- tp1final/funciones.js
+++ tp1final/funciones.js
@@ -15,7 +15,7 @@ function botonDibujo(x, y, ancho, alto) {
 function escribirTitulo(texto, x, y, tam, alinea) {//para ecribir el texto del título y los botones
   stroke(0);
   fill(255)
-  textAlign(CENTER);
+    textAlign(CENTER);
   textSize(tam);
   text(texto, x, y);
 }
@@ -24,4 +24,27 @@ function escribirParrafos (texto, x, y, tam, alinea) {//
   textAlign(CENTER);
   textSize(tam);
   text(texto, x, y);
+}
+
+function botonDibujo2(texto, x, y, ancho, alto) {
+  push();
+  if (botonUso(x, y, ancho, alto)) {
+    fill(126, 216, 31);
+  } else {
+    fill(57, 103, 8);
+  }
+  noStroke();
+  rect(x, y, ancho, alto, alto/4);
+  escribirTitulo2(texto, x+ancho/2, y+alto/2);
+  pop();
+}
+
+function escribirTitulo2(texto, x, y) {//para ecribir el texto del título y los botones
+  push();
+  textAlign(CENTER, CENTER);
+  stroke(0);
+  fill(255)
+  textSize(25);
+  text(texto, x, y);
+  pop();
 }
