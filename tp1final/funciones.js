6869fc113dbd839a2cfb5950410dfc51eecae647
Ana Paz Parietti <166624266+anapazparietti@users.noreply.github.com>
2024-10-17 00:37:09 -0300

    Add files via upload

--- tp1final/funciones.js
+++ tp1final/funciones.js
@@ -7,12 +7,8 @@ function botonUso(x, y, ancho, alto) {
   return mouseX > x && mouseX < x+ancho && mouseY > y && mouseY < y+alto;
 }
 
-function botonDibujo(x, y, ancho, alto) {
-  fill(200);
-  rect(x, y, ancho, alto);
-}
 
-function escribirTitulo(texto, x, y, tam, alinea) {//para ecribir el texto del título y los botones
+function escribirTitulo(texto, x, y, tam, alinea) {//para ecribir el texto del título 
   stroke(0);
   fill(255)
     textAlign(CENTER);
